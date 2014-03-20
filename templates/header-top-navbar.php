06bb26ce33ed9bcfc6661fde8ef2ac6120551624
bexarcreative-daniel <daniel@bexarcreative.com>
2014-03-20 12:30:25 -0500

    Added font-awesome fonts. Added sliced images.

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,11 +1,9 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<header role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+        <i class="fa fa-fw fa-bars"></i>
       </button>
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
@@ -18,4 +16,6 @@
       ?>
     </nav>
   </div>
+
+
 </header>
