e53664af0f05e229e8682f94751a87a731cf8551
bexarcreative-daniel <daniel@bexarcreative.com>
2014-03-31 11:10:24 -0500

    Fixed lead paragraph spacing.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '092745c0c87b27aa4988428f449da698');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '80d964ddd80e59c10f086b0fc520df40');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
