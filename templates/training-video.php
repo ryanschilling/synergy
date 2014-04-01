6a69d6f2326aabafaa114f4e509397262965311e
bexarcreative-daniel <daniel@bexarcreative.com>
2014-03-31 22:16:45 -0500

    Tweaked presentation of training video grids.

--- templates/training-video.php
+++ templates/training-video.php
@@ -4,10 +4,14 @@
       <h3><?php $series = wp_get_post_terms(get_the_ID(), 'training-video-series'); echo empty($series) ? 'Training Video' : $series[0]->name; ?></h3>
     </div>
     <div class="panel-body">
-      <a href="<?=get_permalink($post->ID)?>">
-        <?=get_the_post_thumbnail($post->ID, 'panel-image'); ?>
-        <div class="overlay"><i class="fa fa-fw fa-4x fa-<?=get_field('embed_icon', $post->ID); ?>"></i></div>
-      </a>
+      <?php if(!$img = get_the_post_thumbnail($post->ID, 'panel-image')): ?>
+        <a href="<?=get_permalink($post->ID)?>">
+          <?=get_the_post_thumbnail($post->ID, 'panel-image'); ?>
+          <div class="overlay"><i class="fa fa-fw fa-4x fa-<?=get_field('embed_icon', $post->ID); ?>"></i></div>
+        </a>
+      <?php else: ?>
+        <iframe class="video" src="<?=get_field('training_video_link', $post->ID);?>?controls=2&modestbranding=1&origin=<?=urlencode('http://'.$_SERVER["HTTP_HOST"])?>&showinfo=0&theme=light&rel=0" width="354" height="256" frameborder="0" allowfullscreen></iframe>
+      <?php endif; ?>
     </div>
   </div>
   <div class="description">
