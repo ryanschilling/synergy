bcc950fbf2396db3935d6e05e768fdb057a4f992
bexarcreative-daniel <daniel@bexarcreative.com>
2014-04-07 22:35:56 -0500

    Tweaked styles on gravity forms.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a80834887ff75e722f34b68bd50c86d8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '446398586c718747f3fa8dc520cdf947');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
