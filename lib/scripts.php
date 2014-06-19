4bf8f997d404000851a65c79fc397446c8f2742a
ryanschilling <ryan@ryan-schilling.com>
2014-06-19 11:59:46 -0500

    test synergy updatei

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a80834887ff75e722f34b68bd50c86d8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f61627ca4c4fcb4b9b861924192e01d4');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
